e572abca6644b5c0db0f15d92290c2a9975e380e
Andrey345555 <starcenkoandrej47@gmail.com>
2023-02-05 21:58:57 +0200

    проект

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Searcher } from "./services/api";
+let INITIAL_STATE= {
+  search:"",
+  list:[]
 }
-
-export default App;
+export default class ArticlesList extends Component{
+    state = {
+...INITIAL_STATE
+    }
+handleSubmit = async (event) => {
+console.log(1)
+await event.preventDefault();
+const response = await Searcher(`${event.nativeEvent.path[1].childNodes[0].defaultValue}`)
+console.log(response)
+this.setState({search:event.nativeEvent.path[1].childNodes[0].defaultValue})
+this.setState({list:response})
+}
+inputVal = async (event) =>{
+  const { value } = event.target;
+  this.setState({ search: value });
+}
+render(){
+  return(
+    <div>
+        <input
+          placeholder="search"
+          type="text"
+          name="search"
+          value={this.state.search}
+          onChange={this.inputVal}
+        />
+        
+        <button onClick={this.handleSubmit}>
+          
+          Login
+        </button>
+        {this.state.list.map((data) => {return(
+          <li key={data.objectID}>
+            <h2><a href={data.url} target="blank">{data.title}</a></h2>
+            <p>Автор: {data.author}</p>
+          </li>
+        )})}
+</div>
+  )
+}
+}
