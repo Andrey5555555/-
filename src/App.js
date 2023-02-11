a222aebc21c5a7a234aff30e0918f5cae221c68d
Andrey345555 <starcenkoandrej47@gmail.com>
2023-02-11 18:34:51 +0200

    hooks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import {useState, useRef, useEffect} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+const [Question, setQuestion] = useState('');
+const questionCont = useRef();
+const questionIn = useRef()
+useEffect(() => {
+  setTimeout(() => {
+    questionCont.current.style.display = "flex"  
+    questionIn.current.focus()
+  }, 1000);
+},[]);
+let changeFinder = (event) =>{
+setQuestion(event.target.value)
+}
+useEffect(() => {
+if(Question.toLowerCase() === "так"){
+  questionCont.current.style.display="none"
+}
+}, [Question]);
   return (
+    <>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +38,11 @@ function App() {
         </a>
       </header>
     </div>
+      <div className='question-cont' ref={questionCont}>
+        <label htmlFor="question">Вам подобається React?</label>
+        <input ref={questionIn} onChange={changeFinder} type="text" name="question" id="question"/>
+      </div>
+    </>
   );
 }
 
